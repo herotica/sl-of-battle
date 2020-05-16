b3e947bfa37b1dff8092583eb156d80b6cca2eb7
herotica <three3323-3@yahoo.com>
2020-05-16 15:06:24 +0100

    push test

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code>
         </p>
         <Char />
         <a
