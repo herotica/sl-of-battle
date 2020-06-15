3ca7e33f60d1eb3ae2e3306fb9d9801421da3014
herotica <three3323-3@yahoo.com>
2020-06-15 19:10:50 +0100

    Added additional characters

--- src/combatants/isekai-league/ranks.js
+++ src/combatants/isekai-league/ranks.js
@@ -3,6 +3,16 @@ import AquaIcon from "../../assets/combatants/isekai-league/aqua-icon.png";
 import AquaWin from "../../assets/combatants/isekai-league/aqua-win.png";
 import AquaLose from "../../assets/combatants/isekai-league/aqua-lose.png";
 
+import Darkness from "../../assets/combatants/isekai-league/darkness.png";
+import DarknessIcon from "../../assets/combatants/isekai-league/darkness-icon.png";
+import DarknessWin from "../../assets/combatants/isekai-league/darkness-win.png";
+import DarknessLose from "../../assets/combatants/isekai-league/darkness-lose.png";
+
+import YuukiAsuna from "../../assets/combatants/isekai-league/yuuki-asuna.png";
+import YuukiAsunaIcon from "../../assets/combatants/isekai-league/yuuki-asuna-icon.png";
+import YuukiAsunaWin from "../../assets/combatants/isekai-league/yuuki-asuna-win.png";
+import YuukiAsunaLose from "../../assets/combatants/isekai-league/yuuki-asuna-lose.png";
+
 export default [
   {
     name: "Amateurs",
@@ -61,45 +71,84 @@ export default [
     reqWins: 1,
     combatants: [
       {
-        name: "Kirigaya Suguha",
-        description:
-          "An accomplished kendo practitioner, fighter, and seasoned ALO player.",
-        img: Aqua,
-        icon: AquaIcon,
-        opWinImg: AquaWin,
-        opLoseImg: AquaLose,
+        name: "Darkness",
+        description: "A noble & serious Crusader, and also a masochist.",
+        img: Darkness,
+        icon: DarknessIcon,
+        opWinImg: DarknessWin,
+        opLoseImg: DarknessLose,
         gender: "female",
         isWoman: true,
         hasCock: false,
         race: "Human",
         bodyShape: "curvy",
-        eyeColor: "dark grey",
-        hairColor: "black",
+        eyeColor: "blue",
+        hairColor: "blonde",
         skinColor: "fair",
         height: 2,
         penisSize: 0,
-        breastSize: 3,
-        vaginaSize: 2,
-        anusSize: 1,
-        throatSize: 3,
-        prefAttack: "mouth",
-        prefTarget: "breasts",
+        breastSize: 4,
+        vaginaSize: 3,
+        anusSize: 3,
+        throatSize: 4,
+        prefAttack: "anus",
+        prefTarget: "cock",
         uniqueAttackText:
-          "Suguha hungrily slathers ++++'s breasts with her tongue, eagerly sucking her nipples and savouring their sweet, sweaty taste.",
-        roughPlayLvl: 20,
-        seductionResistance: 10,
-        grapplingProwess: 30,
-        tongueProwess: 25,
-        touchProwess: 25,
+          "Darkness mounts you like an animal, jamming your hard cock deep into her back passage, she screams as sh forces you deep into her.",
+        roughPlayLvl: 40,
+        seductionResistance: 20,
+        grapplingProwess: 20,
+        tongueProwess: 20,
+        touchProwess: 20,
         cockProwess: 15,
         vaginaProwess: 25,
-        anusProwess: 10,
-        touchResistance: 15,
-        breastResistance: 25,
-        mouthResistance: 20,
-        cockResistance: 5,
+        anusProwess: 35,
+        touchResistance: 25,
+        breastResistance: 65,
+        mouthResistance: 10,
+        cockResistance: 35,
         vaginaResistance: 25,
-        anusResistance: 15,
+        anusResistance: 55,
+        orgasmLimit: 3
+      },{
+        name: "Yuuka Asuna",
+        description: "Sub-leader of the Knights of the Blood guild & is a kind and helpful young woman who, cannot abandon another in trouble.",
+        img: YuukiAsuna,
+        icon: YuukiAsunaIcon,
+        opWinImg: YuukiAsunaWin,
+        opLoseImg: YuukiAsunaLose,
+        gender: "female",
+        isWoman: true,
+        hasCock: false,
+        race: "Human",
+        bodyShape: "lithe",
+        eyeColor: "brown",
+        hairColor: "chestnut",
+        skinColor: "pale",
+        height: 2,
+        penisSize: 0,
+        breastSize: 1,
+        vaginaSize: 1,
+        anusSize: 2,
+        throatSize: 1,
+        prefAttack: "touch",
+        prefTarget: "breasts",
+        uniqueAttackText:
+          "Asuna gropes xxxx's breasts roughly, teasing the soft breast-meat and whispering alluring words into her ear.",
+        roughPlayLvl: -10,
+        seductionResistance: 30,
+        grapplingProwess: 25,
+        tongueProwess: 25,
+        touchProwess: 40,
+        cockProwess: 15,
+        vaginaProwess: 25,
+        anusProwess: 15,
+        touchResistance: 35,
+        breastResistance: 65,
+        mouthResistance: 50,
+        cockResistance: 15,
+        vaginaResistance: 25,
+        anusResistance: 25,
         orgasmLimit: 3
       }
     ]
