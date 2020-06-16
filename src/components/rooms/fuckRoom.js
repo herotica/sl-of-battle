1d937613144f99ed61907b088ba31beb05f66abe
herotica <three3323-3@yahoo.com>
2020-06-16 21:23:04 +0100

    Fuck room basic outline

--- src/components/rooms/fuckRoom.js
+++ src/components/rooms/fuckRoom.js
@@ -15,6 +15,7 @@ const FuckTypes = {
 const FuckRoom = observer(() => {
   const [fuckType, setFuckType] = useState(false);
   const [fuckChoice, setFuckChoice] = useState(false);
+  const [fuckEndChoice, setFuckEndChoice] = useState(false);
   const {
     fuckRoomCombatant,
     name,
@@ -22,8 +23,19 @@ const FuckRoom = observer(() => {
     isWoman,
     setRoomSave
   } = useGlobalDataStore();
+
   const LeaveRoom = () => {
     setRoomSave(Rooms.league);
+  };
+  const getRndJesus = textOptArr =>
+    Math.floor(Math.random() * textOptArr.length);
+  const SetFuckChoiceText = textOptArr => {
+    const RNJesus = getRndJesus(textOptArr);
+    setFuckChoice(textOptArr[RNJesus]);
+  };
+  const SetFuckEndChoiceText = textOptArr => {
+    const RNJesus = getRndJesus(textOptArr);
+    setFuckEndChoice(textOptArr[RNJesus]);
   };
 
   return (
@@ -31,34 +43,73 @@ const FuckRoom = observer(() => {
       <RoomHeader title="The Loser Fucking Rooom">
         <Button onClick={LeaveRoom}>Leave</Button>
       </RoomHeader>
-      {!fuckType && (
-        <>
-          <div onClick={() => setFuckType(FuckTypes.dom)}>{FuckTypes.dom}</div>
-          <div onClick={() => setFuckType(FuckTypes.please)}>
-            {FuckTypes.please}
-          </div>
-        </>
-      )}
-      {fuckType && !fuckChoice && (
-        <>
-          <FlexWrap>
-            {" "}
-            {hasCock &&
-              FuckRoomText[fuckType].hasCock.map(option => (
-                <div>{option.displayText}</div>
-              ))}
-            {hasCock &&
-              fuckType === FuckTypes.dom &&
-              FuckRoomText[fuckType].cockBdsm.map(option => (
-                <div>{option.displayText}</div>
-              ))}
-            {isWoman &&
-              FuckRoomText[fuckType].isWoman.map(option => (
-                <div>{option.displayText}</div>
-              ))}
-          </FlexWrap>
-        </>
-      )}
+      <RoomWrap>
+        <ImageWrap>
+          <Image src={fuckRoomCombatant.img} alt="loser" />
+          <Image src={fuckRoomCombatant.opLoseImg} alt="loser" />
+        </ImageWrap>
+        <TextWrap>
+          {!fuckType && (
+            <>
+              <UpperCase onClick={() => setFuckType(FuckTypes.dom)}>
+                {FuckTypes.dom}
+              </UpperCase>
+              <UpperCase onClick={() => setFuckType(FuckTypes.please)}>
+                {FuckTypes.please}
+              </UpperCase>
+            </>
+          )}
+          {fuckType && !fuckChoice && (
+            <>
+              <FlexWrap>
+                {" "}
+                {hasCock &&
+                  FuckRoomText[fuckType].hasCock.map(option => (
+                    <UpperCase
+                      onClick={() => SetFuckChoiceText(option.textOptions)}
+                    >
+                      {option.displayText}
+                    </UpperCase>
+                  ))}
+                {hasCock &&
+                  fuckType === FuckTypes.dom &&
+                  FuckRoomText[fuckType].cockBdsm.map(option => (
+                    <UpperCase
+                      onClick={() => SetFuckChoiceText(option.textOptions)}
+                    >
+                      {option.displayText}
+                    </UpperCase>
+                  ))}
+                {isWoman &&
+                  FuckRoomText[fuckType].isWoman.map(option => (
+                    <UpperCase
+                      onClick={() => SetFuckChoiceText(option.textOptions)}
+                    >
+                      {option.displayText}
+                    </UpperCase>
+                  ))}
+              </FlexWrap>
+            </>
+          )}
+          {fuckChoice && (
+            <>
+              <div>{fuckChoice}</div>
+              {hasCock &&
+                !fuckEndChoice &&
+                FuckRoomText.finishCock.map(option => (
+                  <UpperCase
+                    onClick={() => SetFuckEndChoiceText(option.textOptions)}
+                  >
+                    {option.displayText}
+                  </UpperCase>
+                ))}
+            </>
+          )}
+          {fuckEndChoice && <div>{fuckEndChoice}</div>}
+        </TextWrap>
+      </RoomWrap>
+
+      <Button onClick={LeaveRoom}>Leave</Button>
     </UWrap>
   );
 });
@@ -67,10 +118,34 @@ const UWrap = styled.div`
   margin: 32px;
   overflow-y: auto;
 `;
-
+const RoomWrap = styled.div`
+  display: flex;
+`;
+const ImageWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 12px;
+  padding-right: 12px;
+  border-right: 1px solid grey;
+  text-align: center;
+`;
+const TextWrap = styled.div`
+  margin: 12px;
+`;
+const Image = styled.img`
+  max-width: 320px;
+  margin-bottom: 24px;
+`;
 const FlexWrap = styled.div`
   display: flex;
   flex-wrap: wrap;
+`;
+const UpperCase = styled(Button)`
+  margin: 8px;
+
+  &:first-letter {
+    text-transform: uppercase;
+  }
 `;
 
 export default FuckRoom;
