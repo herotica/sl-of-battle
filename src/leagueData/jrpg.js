2700a3f7762643e62827f85b5c29d79c732df2c0
herotica <three3323-3@yahoo.com>
2020-07-06 20:43:03 +0100

    Added Granblue league

--- src/leagueData/jrpg.js
+++ src/leagueData/jrpg.js
@@ -1,4 +1,5 @@
 import FireEmblem from "../combatants/jrpg/FireEmblem";
+import GranblueFantasy from "../combatants/jrpg/GranblueFantasy";
 import icon from "../assets/logo/jrpg.png";
 
 export default {
@@ -18,21 +19,31 @@ export default {
       name: "Rookies",
       creditsWin: 5,
       pointsWin: 5,
-      combatants: [FireEmblem.corrin, FireEmblem.lucina],
+      combatants: [
+        FireEmblem.corrin,
+        FireEmblem.lucina,
+        GranblueFantasy.vikala,
+        GranblueFantasy.silva,
+      ],
     },
     {
       name: "Amateurs",
       creditsWin: 10,
       pointsWin: 10,
       reqWins: 2,
-      combatants: [FireEmblem.lyn, FireEmblem.lysithea],
+      combatants: [
+        FireEmblem.lyn,
+        FireEmblem.lysithea,
+        GranblueFantasy.ferry,
+        GranblueFantasy.djeeta,
+      ],
     },
     {
       name: "Pro",
       creditsWin: 20,
       pointsWin: 20,
       reqWins: 1,
-      combatants: [FireEmblem.edelgard],
+      combatants: [FireEmblem.edelgard, GranblueFantasy.cagliostro],
     },
   ],
 };
